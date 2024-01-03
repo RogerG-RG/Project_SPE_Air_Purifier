0e320d24d5ce519365c74f0cedf776469f44a9b5
RogerG-RG <roger.rg327@gmail.com>
2024-01-03 18:05:39 +0700

    Final Code

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,10 @@
 #include <Adafruit_SSD1306.h>
 #include <Wire.h>
 #include <SPI.h>
+#include <Firebase_ESP_Client.h>
+
+#define FIREBASE_HOST "https://air-purifier-e1b0d-default-rtdb.asia-southeast1.firebasedatabase.app/"
+#define FIREBASE_AUTH "48glLqNosGJdyqc4cHw7EOjYZrWv9gN413MzMLsM"
 
 #define WIFI_RESET_BUTTON_PIN 23
 #define FAN_PWM_CTRL_PIN 13
@@ -15,7 +19,11 @@
 #define OLED_HEIGHT 64
 #define OLED_RESET -1 //Not used
 
-float pm10, pm25;
+float pm10, pm25, aqivalue;
+String aqiLevelCategory;
+int fanSpeedStatus;
+unsigned long previousMillisFanSpeedStatus = 0;
+unsigned long previousMillisPM = 0;
 int error; 
 char apName[] = "AirBox";
 const int PWM_CHANNEL = 0;
@@ -32,11 +40,15 @@ HardwareSerial port(2); // UART Serial 2 (RX2, TX2) or pin 16, 17. Connect the R
 #endif
 Adafruit_SSD1306 display(OLED_WIDTH, OLED_HEIGHT, &Wire, OLED_RESET);
 
+FirebaseData fbdo;
+FirebaseConfig fbConfig;
+FirebaseData fbdoStream;
+
 void wifiConnect(void * parameter);
 void timedOut(void * parameter);
 void sensorRead(void * parameter); 
 void fanSpeed(void * parameter);
-
+void Firebase_Init(const String& streamPath);
 
 void setup() {
   pinMode(WIFI_RESET_BUTTON_PIN, INPUT_PULLUP);
@@ -61,19 +73,55 @@ void setup() {
   xTaskCreate(wifiConnect, "Wifi Connect", 10000, NULL, 1, NULL);
   xTaskCreate(timedOut, "Timed Out", 10000, NULL, 1, NULL);
   Serial.begin(115200);
+
+  Firebase_Init("cmd");
+  if (!Firebase.ready()) {
+    Serial.println("Firebase initialization failed. Check your credentials and connection.");
+    Firebase_Init("cmd");
+    while (1);
+  }
+  else {
+    Serial.println("Firebase initialized.");
+  }
 }
 
 void fanSpeed(void * parameter) {
   while(1)  {
-    Serial.println("Changing fan speed");
-    ledcWrite(0, 128);
-    delay(3000);
-    ledcWrite(0, 64);
-    delay(3000);
-    ledcWrite(0, 0);
-    Serial.println("Fan speed changed");
+    if(fanSpeedStatus == 0) {
+      ledcWrite(PWM_CHANNEL, 0);
+      Serial.println("Fan Speed changed to 0%");
+    }
+    if(fanSpeedStatus == 1) {
+      ledcWrite(PWM_CHANNEL, 64);
+      Serial.println("Fan Speed changed to 25%");
+    }
+    if(fanSpeedStatus == 2) {
+      ledcWrite(PWM_CHANNEL, 128);
+      Serial.println("Fan Speed changed to 50%");
+    }
+    if(fanSpeedStatus == 3) {
+      ledcWrite(PWM_CHANNEL, 192);
+      Serial.println("Fan Speed changed to 75%");
+    }
     vTaskDelay(pdMS_TO_TICKS(3000));
   }
+
+  // while(fanSpeedStatus == 0)  {
+  //   ledcWrite(PWM_CHANNEL, 0);
+  //   Serial.println("Fan Speed changed to 0%");
+  // }
+  // while(fanSpeedStatus == 1)  {
+  //   ledcWrite(PWM_CHANNEL, 64);
+  //   Serial.println("Fan Speed changed to 25%");
+  // }
+  // while(fanSpeedStatus == 2)  {
+  //   ledcWrite(PWM_CHANNEL, 128);
+  //   Serial.println("Fan Speed changed to 50%");
+  // }
+  // while(fanSpeedStatus == 3)  {
+  //   ledcWrite(PWM_CHANNEL, 192);
+  //   Serial.println("Fan Speed changed to 75%");
+  // }
 }
 
 void sensorRead(void * parameter) {
@@ -85,6 +133,60 @@ void sensorRead(void * parameter) {
       Serial.print(pm25);
       Serial.print("\nPM10: ");
       Serial.println(pm10);
+
+      if(pm25 >= 0.0 && pm25 <= 12.0) {
+        aqivalue = map(pm25, 0.0, 12.0, 0, 50);
+        Serial.print("\nAQI Value: ");
+        Serial.println(aqivalue);
+        aqiLevelCategory = "Good";
+        Serial.print("\nAir Quality: ");
+        Serial.println(aqiLevelCategory);
+      }
+
+      if(pm25 >= 12.1 && pm25 <= 35.4)  {
+        aqivalue = map(pm25, 12.1, 35.4, 51, 100);
+        Serial.print("\nAQI Value: ");
+        Serial.println(aqivalue);
+        aqiLevelCategory = "Moderate";
+        Serial.print("\nAir Quality: ");
+        Serial.println(aqiLevelCategory);
+      }
+
+      if(pm25 >= 35.5 && pm25 <= 55.4)  {
+        aqivalue = map(pm25, 35.5, 55.4, 101, 150);
+        Serial.print("\nAQI Value: ");
+        Serial.println(aqivalue);
+        aqiLevelCategory = "Unhealthy for Sensitive Groups";
+        Serial.print("\nAir Quality: ");
+        Serial.println(aqiLevelCategory);
+      }
+
+      if(pm25 >= 55.5 && pm25 <= 150.4)  {
+        aqivalue = map(pm25, 55.5, 150.4, 151, 200);
+        Serial.print("\nAQI Value: ");
+        Serial.println(aqivalue);
+        aqiLevelCategory = "Unhealthy";
+        Serial.print("\nAir Quality: ");
+        Serial.println(aqiLevelCategory);
+      }
+
+      if(pm25 >= 150.5 && pm25 <= 250.4)  {
+        aqivalue = map(pm25, 150.5, 250.4, 201, 300);
+        Serial.print("\nAQI Value: ");
+        Serial.println(aqivalue);
+        aqiLevelCategory = "Very Unhealthy";
+        Serial.print("\nAir Quality: ");
+        Serial.println(aqiLevelCategory);
+      }
+
+      if(pm25 >= 250.5 && pm25 <= 500.4)  {
+        aqivalue = 300.1;
+        Serial.print("\nAQI Value: ");
+        Serial.println(aqivalue);
+        aqiLevelCategory = "Hazardous";
+        Serial.print("\nAir Quality: ");
+        Serial.println(aqiLevelCategory);
+      }
     
       display.clearDisplay();
       display.setCursor(0,0);
@@ -92,7 +194,16 @@ void sensorRead(void * parameter) {
       display.print(pm25);
       display.print("\nPM 10: ");
       display.print(pm10);
+      display.print("\nAQI:");
+      display.print(aqivalue);
+      display.print("\nAir Quality:\n");
+      display.print(aqiLevelCategory);
       display.display();
+
+      Firebase.RTDB.setFloat(&fbdo, "/AQI/pm25", pm25);
+      Firebase.RTDB.setFloat(&fbdo, "/AQI/pm10", pm10);
+      Firebase.RTDB.setFloat(&fbdo, "/AQI/value", aqivalue);
+      Serial.println("Firebase updated");
       delay(1000);
     } else {
       Serial.println("Error reading from SDS011\n");
@@ -131,11 +242,77 @@ void timedOut(void * parameter) {
   }
 }
 
+void Firebase_Init(const String& streamPath)  {
+  FirebaseAuth fbAuth;
+  fbConfig.host = FIREBASE_HOST;
+  fbConfig.signer.tokens.legacy_token = FIREBASE_AUTH;
+  Firebase.begin(&fbConfig, &fbAuth);
+  Firebase.reconnectWiFi(true);
+
+  fbdo.setResponseSize(2048);
+  Firebase.RTDB.setwriteSizeLimit(&fbdo, "medium");
+  while (!Firebase.ready())
+  {
+    Serial.println("Connecting to firebase...");
+    delay(1000);
+  }
+  String path = streamPath;
+  if (Firebase.RTDB.beginStream(&fbdoStream, path.c_str()))
+  {
+    Serial.print("Firebase stream on ");
+    Serial.println(path);
+  }
+  else
+    Serial.print("Firebase stream failed: ");
+    Serial.println(fbdoStream.errorReason().c_str());
+}
+
 void loop() {
   if(digitalRead(WIFI_RESET_BUTTON_PIN) == LOW) {
     wifiManager.resetSettings();
     ESP.restart();
   }
+
+  if (!Firebase.ready()) {
+    Serial.println("Firebase initialization failed. Check your credentials and connection.");
+    Firebase_Init("cmd");
+    while (1);
+  }
+  else {
+    Serial.println("Firebase initialized.");
+    delay(3000);
+  }
+
+  if(Firebase.ready() && (millis() - previousMillisFanSpeedStatus > 3000 || previousMillisFanSpeedStatus == 0))  {
+      previousMillisFanSpeedStatus = millis();
+
+      if(Firebase.RTDB.getInt(&fbdo, "/fan/speed")) {
+        if(fbdo.dataType() == "int") {
+          fanSpeedStatus = fbdo.intData();
+          Serial.println(fanSpeedStatus);
+
+          if(fanSpeedStatus == 0) {
+          ledcWrite(PWM_CHANNEL, 0);
+          Serial.println("Fan Speed changed to 0%");
+          }
+          if(fanSpeedStatus == 1) {
+            ledcWrite(PWM_CHANNEL, 64);
+            Serial.println("Fan Speed changed to 25%");
+          }
+          if(fanSpeedStatus == 2) {
+            ledcWrite(PWM_CHANNEL, 128);
+            Serial.println("Fan Speed changed to 50%");
+          }
+          if(fanSpeedStatus == 3) {
+            ledcWrite(PWM_CHANNEL, 192);
+            Serial.println("Fan Speed changed to 75%");
+          }
+        }
+      }
+      else  {
+        Serial.println(fbdo.errorReason());
+      }
+    }
   // digitalWrite(ONBOARD_LED_PIN, HIGH);
   // delay(1000);
   // digitalWrite(ONBOARD_LED_PIN, LOW);
